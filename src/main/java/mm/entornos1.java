92e2225522a70e063a4c73020ba30a028f3c9747
1DAM-javierrei211 <jreicam0501@g.educaand.es>
2024-10-15 20:15:39 +0200

    Update entornos1.java
    actualización

--- src/main/java/mm/entornos1.java
+++ src/main/java/mm/entornos1.java
@@ -2,6 +2,6 @@ package mm;
 
 public class entornos1 {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundoolo,");
     }
 }
